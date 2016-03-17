850246dec35f805494c8647dd7fe14e0fb7c0c01
Justin Lafferty <lafferjm@gmail.com>
2016-03-17 18:54:47 -0500

    Removing unneeded resources and boilerplate code.

---
+++ Testing/Classes/TestScene.cpp
@@ -0,0 +1,33 @@
+#include "TestScene.h"
+
+using namespace cocos2d;
+
+Scene* TestScene::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = TestScene::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool TestScene::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    Size visibleSize = Director::getInstance()->getVisibleSize();
+    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+}
