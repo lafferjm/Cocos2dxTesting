6f79778274afb048a52d2e69031b0721d23cb35f
Justin Lafferty <lafferjm@gmail.com>
2016-03-18 20:42:13 -0500

    Random rotation direction added.

--- Testing/Classes/Cross.cpp
+++ Testing/Classes/Cross.cpp
@@ -8,7 +8,14 @@ Cross::Cross(int x, int y, Layer* layer)
     mCrossSprite = Sprite::create("Cross.png");
     mCrossSprite->setPosition(x, y);
     
-    RotateBy *rotateByAction = RotateBy::create(2.0, -360.0);
+    float rotateAmount = -360.0;
+    
+    if(RandomHelper::random_int(1, 2) == 2)
+    {
+        rotateAmount = 360.0;
+    }
+    
+    RotateBy *rotateByAction = RotateBy::create(2.0, rotateAmount);
     mCrossSprite->runAction(RepeatForever::create(rotateByAction));
     
     layer->addChild(mCrossSprite);
